9dde5cb8eaa9708ce3a79047ffc9f56443f58a5f
Jelle Buurman <30508199+ItsGelle@users.noreply.github.com>
2018-07-10 10:51:08 +0200

    Ajax4

---
+++ opdracht3/dbConnect.php
@@ -0,0 +1,8 @@
+<?php
+$servername = 'localhost';
+$username = '25158_root';
+$password = '25158_root';
+$database = '25158_db';
+
+
+$con = mysqli_connect($servername,$username,$password,$database);
